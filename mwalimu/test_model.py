0059800706508fd275aa9dfa5a567ac56e9f353a
Penina Wanjiru <penina.wanjiru@andela.com>
2016-08-23 14:37:16 +0300

    [Feature] write user model tests

---
+++ mwalimu/test_model.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from mwalimu.models import User
+
+
+class TeacherAppTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='user', password='password')
+
+    def test_user_signup(self):
+        self.assertEqual(self.user.get_username(), 'user')
+        self.assertIsInstance(self.user, User)
